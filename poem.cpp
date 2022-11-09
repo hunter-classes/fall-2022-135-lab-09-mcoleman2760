1c577e4f6d6de432c27a25908caf5cc7448a56b1
mcoleman2760 <104951107+mcoleman2760@users.noreply.github.com>
2022-11-09 14:46:49 -0500

    Create poem.cpp

---
+++ poem.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+using namespace std;
+
+string * createAPoemDynamically() {
+    string *p = new string;
+    *p = "Roses are red, violets are blue";
+    return p;
+}
+
+int main() {
+    while(true) {
+        string *p;
+        p = createAPoemDynamically();
+
+        
+    if (!p)
+    {
+    cout << " Memory allocation failed\n";
+    }   
+    cout<<*p<<"\n";
+    
+    delete p;
+
+    }
+}
