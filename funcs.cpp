55f74ad9dd594e2215a698861b4a9e4e42049225
mcoleman2760 <104951107+mcoleman2760@users.noreply.github.com>
2022-11-10 10:40:04 -0500

    Update funcs.cpp

--- funcs.cpp
+++ funcs.cpp
@@ -1,5 +1,66 @@
 #include <iostream>
 #include "funcs.h"
+#include "coord3d.h"
+#include <cmath>
 
-// add functions here
+double length (Coord3D *p){
 
+double len = sqrt(pow(p->x,2) + pow(p->y,2) + pow(p->z,2)); 
+
+return len;
+
+}
+
+Coord3D * fartherFromOrigin(Coord3D *p1, Coord3D *p2){
+
+ 
+ if (length(p1) > length(p2)){
+
+    return p1;
+
+ }
+
+ else {
+    return p2;
+ }
+
+
+
+}
+
+void move(Coord3D *ppos, Coord3D *pvel, double dt) {
+
+Coord3D &P =*ppos;
+Coord3D &V =*pvel;
+
+
+P.x = P.x + V.x * dt;
+P.y = P.y + V.y * dt;
+P.z = P.z + V.z * dt;
+
+ 
+
+}
+
+Coord3D* createCoord3D(double x, double y, double z){
+
+Coord3D* ncoord = new Coord3D;
+
+ncoord->x = x;
+
+ncoord->y = y;
+
+ncoord->z = z;
+
+return ncoord;
+
+
+}
+
+void deleteCoord3D(Coord3D *p){
+
+delete p;
+
+
+
+}
